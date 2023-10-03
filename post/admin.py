0e3f517b6ada06b430cc4714e0f33b36a30a405d
jod431047 <jod431047@gmail.com>
2023-10-03 21:56:46 +0200

    add models in admin & views

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['name','price']
+    list_filter = ['name','price']
+    search_fields = ['name','price']
+    
+admin.site.register(Post,PostAdmin)
+    
--- post/views.py
+++ post/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import *
+from django.views import generic
 
 # Create your views here.
+class PostList(generic.ListView):
+    model  = Post
