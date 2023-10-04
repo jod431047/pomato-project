dd94684c15472f2f653592885e9fd5c32d5c3e23
jod431047 <jod431047@gmail.com>
2023-10-04 23:44:12 +0200

    add include urls

--- post/models.py
+++ post/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Post(models.Model):
     name = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=8,decimal_places=2)
+    price = models.FloatField()
     image = models.ImageField(upload_to='post')
     
     
     def __str__ (self):
-        return self.name
+        return f"{self.name} - {self.price}"
