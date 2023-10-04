8a2a67f8f5ca7f9316a6c9b9044aff75afd2945e
jod431047 <jod431047@gmail.com>
2023-10-04 22:46:44 +0200

    add frontend

--- post/admin.py
+++ post/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import *
+from .models import Post
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     list_display = ['name','price']
--- post/models.py
+++ post/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     name = models.CharField(max_length=200)
-    puplish_date = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=8,decimal_places=2)
     image = models.ImageField(upload_to='post')
     
